3fa36a3d1cb6aac5291d44cfc36ced951628af78
KoutarouYabe <idolm@ster.pw>
2018-01-22 03:53:22 +0900

    a

--- src/main/kotlin/moe/linux/hello/HelloApplet.kt
+++ src/main/kotlin/moe/linux/hello/HelloApplet.kt
@@ -5,6 +5,12 @@ import com.github.salomonbrys.kodein.KodeinAware
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.lazy
 import moe.linux.hello.di.AppModule
+import org.jbox2d.collision.AABB
+import org.jbox2d.collision.PolygonShape
+import org.jbox2d.collision.ShapeDef
+import org.jbox2d.common.Vec2
+import org.jbox2d.dynamics.BodyDef
+import org.jbox2d.dynamics.World
 import org.slf4j.Logger
 import processing.core.PApplet
 import processing.core.PConstants
@@ -26,8 +32,8 @@ open class HelloApplet : KodeinAware, PApplet() {
     companion object {
         fun main() {
             println("start processing")
-            PApplet.main(HelloApplet::class.java)
-            PApplet.main(HelloApplet2::class.java)
+//            PApplet.main(HelloApplet::class.java)
+            PApplet.main(HelloApplet3::class.java)
         }
     }
 
@@ -94,4 +100,63 @@ class HelloApplet2 : HelloApplet() {
     override fun draw() {
         super.draw()
     }
+}
+
+
+class HelloApplet3 : HelloApplet() {
+    override fun setup() {
+        super.setup()
+//        text = TextB()
+    }
+
+    override fun settings() {
+        super.settings()
+    }
+
+    override fun draw() {
+//        super.draw()
+        ellipse(200F, 200F, 380F, 380F)
+    }
+}
+
+open class AppletCore : KodeinAware, PApplet() {
+    override val kodein: Kodein by Kodein.lazy {
+        import(AppModule())
+    }
+
+    private val logger: Logger = kodein.instance()
+
+    lateinit var world: World
+
+    override fun setup() {
+        super.setup()
+        logger.info("setup")
+        frameRate(60f)
+    }
+
+    override fun settings() {
+        super.settings()
+        logger.info("setting")
+        size(400, 400)
+        val gravity = Vec2(0F, 9.8F)
+        val world = World(AABB(), gravity, false)
+
+        val groundBody = world.createBody(
+                BodyDef().apply {
+                    this.position.set(0F, 0F)
+                })
+        val groundBox = PolygonShape(ShapeDef()).also {
+        }
+    }
+
+    override fun draw() {
+        fill(204F, 90F)
+        rect(0F, 0F, width.toFloat(), height.toFloat())
+        fill(0F, 102F)
+        ellipse(mouseX.toFloat(), mouseY.toFloat(), 9F, 9F)
+    }
+}
+
+fun main(args: Array<String>): Unit {
+    PApplet.main(AppletCore::class.java)
 }
